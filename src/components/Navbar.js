7c13b6c089940d4d87c2bffaa97ace4660cbbed1
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-03-16 22:36:48 -0400

    add search bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { makeStyles } from "@mui/styles";
+import { alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -10,6 +11,21 @@ const useStyles = makeStyles(() => ({
   toolbar: {
     display: "flex",
     justifyContent: "space-between",
+  },
+  search: {
+    display: "flex",
+    alignItems: "center",
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    width: "50%",
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+  },
+  input: {
+    color: "white",
+    marginLeft: theme.spacing(1),
   },
   logoLg: {
     display: "none",
@@ -38,7 +54,10 @@ export default function NavBar() {
         </Typography>
         <div className={classes.search}>
           <SearchIcon />
-          <InputBase placeholder="Search..." />
+          <InputBase placeholder="Search..." className={classes.input} />
+        </div>
+        <div className={classes.icons}>
+          
         </div>
       </Toolbar>
     </AppBar>
