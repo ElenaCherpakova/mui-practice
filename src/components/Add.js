bcced48d413509a8dcd623fa932387040d4f2cfe
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-03-20 00:56:16 -0400

    add fields in appeared window, add close button

--- src/components/Add.js
+++ src/components/Add.js
@@ -4,6 +4,13 @@ import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import Modal from "@mui/material/Modal";
 import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormLabel from "@mui/material/FormLabel";
+import Button from "@mui/material/Button";
 import { useState } from "react";
 import { theme } from "../theme";
 
@@ -42,7 +49,87 @@ export default function Add() {
         </Fab>
       </Tooltip>
       <Modal open={open}>
-        <Container className={classes.container}>More Details</Container>
+        <Container className={classes.container}>
+          <form className={classes.form} autocomplete="off">
+            <div className={classes.item}>
+              <TextField
+                style={{ width: "100%" }}
+                id="standard-basic"
+                label="Title"
+                size="small"
+                variant="standard"
+              />
+            </div>
+            <div className={classes.item}>
+              <TextField
+                style={{ width: "100%" }}
+                id="outlined-multiline-static"
+                multiline
+                rows={4}
+                placeholder="Tell your story..."
+                label="Description"
+                size="small"
+                variant="outlined"
+              />
+            </div>
+            <div className={classes.item}>
+              <TextField select label="Visibility" value="Public">
+                <MenuItem value="Public">Public</MenuItem>
+                <MenuItem value="Private">Private</MenuItem>
+                <MenuItem value="Unlisted">Unlisted</MenuItem>
+              </TextField>
+            </div>
+            <div>
+              <FormLabel id="demo-radio-buttons-group-label">
+                Who can leave comments
+              </FormLabel>
+
+              <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="female"
+                name="radio-buttons-group"
+              >
+                <FormControlLabel
+                  value="Everybody"
+                  control={<Radio size="small" />}
+                  label="Everybody"
+                />
+                <FormControlLabel
+                  value="My Friends"
+                  control={<Radio size="small" />}
+                  label="My Friends"
+                />
+                <FormControlLabel
+                  value="Nobody"
+                  control={<Radio size="small" />}
+                  label="Nobody"
+                />
+                <FormControlLabel
+                  value="Custom"
+                  disabled
+                  control={<Radio size="small" />}
+                  label="Custom (Premium)"
+                />
+              </RadioGroup>
+            </div>
+            <div className={classes.item}>
+              <Button
+                variant="outlined"
+                color="primary"
+                style={{ marginRight: 10 }}
+              >
+                Create
+              </Button>
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={() => setOpen(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </Container>
       </Modal>
     </>
   );
